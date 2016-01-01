8cc9a1b8c1de5cb3bd2c9ae79492830be5d71877
Rauko <rauko1753@gmail.com>
2015-12-31 17:07:23 -0700

    Stubbed Entity-Component model

--- core/model.go
+++ core/model.go
@@ -1,7 +1,35 @@
 package core
 
+// Tile is an Entity representing a single square in a map.
 type Tile struct {
 	Face     Glyph
 	Pass     bool
 	Adjacent map[Offset]*Tile
+}
+
+// Handle implements Entity for Tile
+func (*Tile) Handle(Event) {
+}
+
+// Event is a message sent to an Entity.
+type Event interface{}
+
+// Component processes Events for an Entity.
+type Component interface {
+	Process(Event)
+}
+
+// Entity is a single game object, typically a collection of Component.
+type Entity interface {
+	Handle(Event)
+}
+
+// ComponentSlice is a simple Entity which is a slice of Components.
+type ComponentSlice []Component
+
+// Handle sends an event to each Component in order.
+func (e ComponentSlice) Handle(v Event) {
+	for _, c := range e {
+		c.Process(v)
+	}
 }
