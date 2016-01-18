af9d69c14e35e15ec6427776f1f3579fb6578377
Rauko <rauko1753@gmail.com>
2016-01-18 12:02:39 -0700

    Added ListSelect

--- core/ui.go
+++ core/ui.go
@@ -188,3 +188,32 @@ func (w *LogWidget) Update() {
 		msg.Seen = true
 	}
 }
+
+func ListSelect(title string, items []interface{}) (index int, ok bool) {
+	state := TermSave()
+	defer state.Restore()
+
+	rows := []string{title}
+	cols := len(title)
+	for i, item := range items {
+		row := fmt.Sprintf("%c) %v", i+'a', item)
+		rows = append(rows, row)
+		cols = Max(cols, len(row))
+	}
+
+	for y, row := range rows {
+		for x, ch := range row {
+			TermDraw(x, y, Glyph{ch, ColorWhite})
+		}
+		for x := len(row); x < cols; x++ {
+			TermDraw(x, y, Glyph{' ', ColorWhite})
+		}
+	}
+	TermRefresh()
+
+	index = int(GetKey() - 'a')
+	if index < 0 || index >= len(items) {
+		return 0, false
+	}
+	return index, true
+}
